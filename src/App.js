420e6218997d0e730c118353b4174395ea24e2c9
Muhammad1327 <ismail1327.ali@gmail.com>
2023-02-19 01:03:34 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Todo from "./components/todoreact/todo";
+import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+    <Temp></Temp>
+      {/* <Todo /> */}
+    </>
+  );
+};
+
+export default App;
